6ba784176f45c32a65199fb0191b816b2d4e840d
kwaikar <wkanchan@gmail.com>
2016-10-11 16:37:10 -0500

    fixing crypto code

--- src/main/scala/edu/utd/security/blueray/Security.scala
+++ src/main/scala/edu/utd/security/blueray/Security.scala
@@ -7,12 +7,19 @@ import javax.crypto.SecretKey
 import javax.crypto.SecretKeyFactory
 import javax.crypto.spec.PBEKeySpec
 import javax.crypto.spec.PBEParameterSpec
+import javax.crypto.spec.IvParameterSpec
+import java.security.spec.AlgorithmParameterSpec
+import java.security.AlgorithmParameters
 
 object Security {
 
+  var cipherEncrypt: Cipher = _;
+  val ALGORITHM = "PBEWithHmacSHA256AndAES_128";
   var salt: Array[Byte] = Array[Byte](
     0xc7.toByte, 0x73.toByte, 0x21.toByte, 0x8c.toByte,
     0x7e.toByte, 0xc8.toByte, 0xee.toByte, 0x99.toByte)
+
+  val pbeParamSpec: PBEParameterSpec = new PBEParameterSpec(salt, 10)
 
   val password = "ADMIN_1_PASSWORD"
 
@@ -21,55 +28,34 @@ object Security {
     val decryptedText = Security.decrypt(encryptedText)
   }
 
-  def generateAndStoreMasterKey(admin1Password:String, admin2Password:String)
-  {
+  def generateAndStoreMasterKey(admin1Password: String, admin2Password: String) {
     // This method would write encryption key to hdfs file
   }
   def decrypt(encryptedText: String): String =
     {
 
-      var count = 20;
-      // Create PBE parameter set
-
-      val pbeKey= getSecretEncryptionKey();
-
-      // Create PBE Cipher
-      var pbeParamSpec: PBEParameterSpec = new PBEParameterSpec(salt, count)
-      var pbeCipher: Cipher = Cipher.getInstance("PBEWithMD5AndDES");
-      pbeCipher.init(Cipher.DECRYPT_MODE, pbeKey, pbeParamSpec);
-
-      // Encrypt the cleartext
-      var deCipheredText: Array[Byte] = pbeCipher.doFinal(Base64.getDecoder.decode(encryptedText));
+      val pbeKey = getSecretEncryptionKey();
+      var pbeCipherDecrypt: Cipher = Cipher.getInstance(pbeKey.getAlgorithm);
+      pbeCipherDecrypt.init(Cipher.DECRYPT_MODE, pbeKey, cipherEncrypt.getParameters);
+      val encryptedBytes = Base64.getDecoder.decode(encryptedText);
+      var deCipheredText: Array[Byte] = pbeCipherDecrypt.doFinal(encryptedBytes);
+      println("--->" + new String(deCipheredText))
       new String(deCipheredText)
-
     }
 
-  def getSecretEncryptionKey() = { 
-
+  def getSecretEncryptionKey() = {
     val pbeKeySpec: PBEKeySpec = new PBEKeySpec(password.toCharArray());
-    val keyFac: SecretKeyFactory = SecretKeyFactory.getInstance("PBEWithMD5AndDES");
+    val keyFac: SecretKeyFactory = SecretKeyFactory.getInstance(ALGORITHM);
     val pbeKey: SecretKey = keyFac.generateSecret(pbeKeySpec);
     pbeKey
   }
+
   def encrypt(plainText: String): String =
     {
-
-      // Salt
-
-      // Iteration count
-      var count = 20;
-
-      // Create PBE parameter set
-      val pbeKey= getSecretEncryptionKey();
-
-      // Create PBE Cipher
-      var pbeCipher: Cipher = Cipher.getInstance("PBEWithMD5AndDES");
-
-      // Initialize PBE Cipher with key and parameters
-      var pbeParamSpec: PBEParameterSpec = new PBEParameterSpec(salt, count)
-      pbeCipher.init(Cipher.ENCRYPT_MODE, pbeKey, pbeParamSpec);
-
-      var cipherText: Array[Byte] = pbeCipher.doFinal(plainText.getBytes);
+      val pbeKey = getSecretEncryptionKey();
+      cipherEncrypt = Cipher.getInstance(pbeKey.getAlgorithm);
+      cipherEncrypt.init(Cipher.ENCRYPT_MODE, pbeKey, pbeParamSpec);
+      var cipherText: Array[Byte] = cipherEncrypt.doFinal(plainText.getBytes);
       Base64.getEncoder().encodeToString(cipherText)
     }
 }
